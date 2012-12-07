e41dc4e7c24e6bef5c2d684d647896895f2ac40f
uu59 <k@uu59.org>
2012-12-08 03:22:28 +0900

    control repository

--- cron.bash
+++ cron.bash
@@ -6,6 +6,10 @@ DIR=$(cd $(dirname $0); pwd)
 cd $DIR
 ./bin/fetch_all
 ./bin/crawl
+
+# sometimes, confused git leaks some files in $DIR
+git reset --hard HEAD
+
 ./bin/generate_json
 git add -u
 git commit -m "update data"
